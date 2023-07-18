ba7a09c8af10467c7fedf99c954b1a18c1aa3b5b
desktop_pc <example@example.com>
2023-07-18 14:03:40 +0800

    tongbu

--- springMVC/springmvc-01-servlet/src/main/java/org/zxb/servelt/HelloServlet.java
+++ springMVC/springmvc-01-servlet/src/main/java/org/zxb/servelt/HelloServlet.java
@@ -9,6 +9,19 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 获取前端参数
+        String method  = req.getParameter("");
+        if(method.equals("add")){
+            req.getSession().setAttribute("msg", "执行了add方法");
+        }
+        if(method.equals("delete")){
+            req.getSession().setAttribute("msg", "执行了delete方法");
+        }
+
+        // 调用业务层
+
+        // 视图转发或者重定向
+        req.getRequestDispatcher("/WEB-INF/jsp/test.jsp").forward(req, resp);
 
     }
 
